6987555a0ab31a7796732f3779ff4b7517d1b40e
Guilherme <gui-sperandio@hotmail.com>
2020-10-13 00:33:14 -0300

    novas atualizações

--- Home/index.js
+++ Home/index.js
@@ -1,4 +1,8 @@
+const teste = document.querySelector("#jorge");
 
+window.addEventListener('load', () => {
+    teste1();
+});
 
 function onSubmit(event){
   
@@ -9,6 +13,7 @@ function onSubmit(event){
          return response.json()
     })
         .then(data => {
+        console.log(data);
         const ul = document.querySelector("#jorge");
         const lis = document.querySelectorAll("#jorge li");
         const pokemonArr = [...lis];
@@ -18,6 +23,8 @@ function onSubmit(event){
          
         const newPoke = document.createElement("li");
         newPoke.className = "pokecard";
+        const link = document.createElement("a");
+        link.setAttribute('href', "https://www.google.com.br/");
         const newPokediv = document.createElement("div");
         newPokediv.className = "pokeinfo";
         const idPoke = document.createElement("p");
@@ -27,10 +34,20 @@ function onSubmit(event){
 
         newPokediv.appendChild(idPoke);
         newPokediv.appendChild(namePoke);
+        var i = 0;
         data.types.map((currentType)=> {
+            
             const typePoke = document.createElement("span");
             typePoke.innerText = currentType.type.name;
             newPokediv.appendChild(typePoke);
+            if(i == 0 ){
+                changeColor(currentType.type.name,typePoke,newPoke);
+                i = i+ 1;
+            }
+            else {
+                changeColorSpan(currentType.type.name,typePoke);
+            }
+            
             return currentType.type.name
         });
         const imagePoke = document.createElement("img");
@@ -41,11 +58,17 @@ function onSubmit(event){
         newPoke.appendChild(newPokediv);
 
         if(data.id < 650){
+
             newPoke.appendChild(imagePoke);
         }
-        
+        //verificar depois pois ficou feio trazer a imagem de 404 quando n acha o poke
+        //else{
+          //  imagePoke.setAttribute('src', `../assets/notfoundpoke.jpg`);
+            //newPoke.appendChild(imagePoke);
+        //}
+        link.appendChild(newPoke);
 
-        ul.appendChild(newPoke);
+        ul.appendChild(link);
 
         const checkImage = document.querySelector(".imagepoke");
         if(checkImage){
@@ -90,7 +113,250 @@ function onSubmit(event){
         return data
         
     });
+    
 }
+
+
+function changeColor(type,typePoke,newPoke){
+    console.log(type);
+    switch(type){
+        case 'grass':
+            newPoke.style.backgroundColor = "#63bb5b";
+            typePoke.style.backgroundColor = "#a7db8d"; 
+        break;
+        case 'normal':
+            newPoke.style.backgroundColor = "#9099a1";
+            typePoke.style.backgroundColor = "#c6c6a7"; 
+        break;
+        case 'fighting':
+            newPoke.style.backgroundColor = "#ce4069";
+            typePoke.style.backgroundColor = "#d67873"; 
+        break;
+        case 'flying':
+            newPoke.style.backgroundColor = "#8fa8dd";
+            typePoke.style.backgroundColor = "#c6b7f5"; 
+        break;
+        case 'poison':
+            newPoke.style.backgroundColor = "#ab6ac8";
+            typePoke.style.backgroundColor = "#c183c1"; 
+        break;
+        case 'ground':
+            newPoke.style.backgroundColor = "#d97746";
+            typePoke.style.backgroundColor = "#ebd69d"; 
+        break;
+        case 'rock':
+            newPoke.style.backgroundColor = "#c7b78b";
+            typePoke.style.backgroundColor = "#d1c17d"; 
+        break;
+        case 'bug':
+            newPoke.style.backgroundColor = "#90c12c";
+            typePoke.style.backgroundColor = "#c6d16e"; 
+        break;
+        case 'ghost':
+            newPoke.style.backgroundColor = "#5269ac";
+            typePoke.style.backgroundColor = "#a292bc"; 
+        break;
+        case 'steel':
+            newPoke.style.backgroundColor = "#5a8ea1";
+            typePoke.style.backgroundColor = "#d1d1e0"; 
+        break;
+        case 'fire':
+            newPoke.style.backgroundColor = "#ff9c54";
+            typePoke.style.backgroundColor = "##f5ac78"; 
+        break;
+        case 'water':
+            newPoke.style.backgroundColor = "#4d90d5";
+            typePoke.style.backgroundColor = "#9db7f5"; 
+        break;
+        case 'electric':
+            newPoke.style.backgroundColor = "#f3d23b";
+            typePoke.style.backgroundColor = "#fae078"; 
+        break;
+        case 'psychic':
+            newPoke.style.backgroundColor = "#f97176";
+            typePoke.style.backgroundColor = "#fa92b2"; 
+        break;
+        case 'ice':
+            newPoke.style.backgroundColor = "#74cec0";
+            typePoke.style.backgroundColor = "#bce6e6"; 
+        break;
+        case 'dragon':
+            newPoke.style.backgroundColor = "#0a6dc4";
+            typePoke.style.backgroundColor = "#a27dfa"; 
+        break;
+        case 'dark':
+            newPoke.style.backgroundColor = "#5a5366";
+            typePoke.style.backgroundColor = "#a29288"; 
+        break;
+        case 'fairy':
+            newPoke.style.backgroundColor = "#ec8fe6";
+            typePoke.style.backgroundColor = "#f4bdc9"; 
+        break;
+    default:
+        console.log("teste");   
+    }
+}
+
+
+
+function changeColorSpan(type,typePoke){
+    switch(type){
+        case 'grass':
+            
+            typePoke.style.backgroundColor = "#a7db8d"; 
+        break;
+        case 'normal':
+            
+            typePoke.style.backgroundColor = "#c6c6a7"; 
+        break;
+        case 'fighting':
+            
+            typePoke.style.backgroundColor = "#d67873"; 
+        break;
+        case 'flying':
+            
+            typePoke.style.backgroundColor = "#c6b7f5"; 
+        break;
+        case 'poison':
+            
+            typePoke.style.backgroundColor = "#c183c1"; 
+        break;
+        case 'ground':
+            
+            typePoke.style.backgroundColor = "#ebd69d"; 
+        break;
+        case 'rock':
+            
+            typePoke.style.backgroundColor = "#d1c17d"; 
+        break;
+        case 'bug':
+            
+            typePoke.style.backgroundColor = "#c6d16e"; 
+        break;
+        case 'ghost':
+            
+            typePoke.style.backgroundColor = "#a292bc"; 
+        break;
+        case 'steel':
+            
+            typePoke.style.backgroundColor = "#d1d1e0"; 
+        break;
+        case 'fire':
+            
+            typePoke.style.backgroundColor = "##f5ac78"; 
+        break;
+        case 'water':
+            
+            typePoke.style.backgroundColor = "#9db7f5"; 
+        break;
+        case 'electric':
+            typePoke.style.backgroundColor = "#fae078"; 
+        break;
+        case 'psychic':
+            
+            typePoke.style.backgroundColor = "#fa92b2"; 
+        break;
+        case 'ice':
+            
+            typePoke.style.backgroundColor = "#bce6e6"; 
+        break;
+        case 'dragon':
+            
+            typePoke.style.backgroundColor = "#a27dfa"; 
+        break;
+        case 'dark':
+            
+            typePoke.style.backgroundColor = "#a29288"; 
+        break;
+        case 'fairy':
+            
+            typePoke.style.backgroundColor = "#f4bdc9"; 
+        break;
+    default:
+        console.log("teste");   
+    }
+}
+
+
+let global = 0;
+function teste1(){
+    var x = 0 ;
+    //for(x=1; x < 650; x++){
+            fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${global}&limit=20`)
+            .then(response =>{
+            return response.json()
+        })
+            .then(data => {
+                console.log(data);
+           
+            for(x =0 ; x <20 ; x++){
+                const url = data.results[x].url;
+                fetch(url)
+                .then(response => {
+                    return response.json()
+                })
+                .then (data => {
+                    const ul = document.querySelector("#jorge");
+                    const newPoke = document.createElement("li");
+                    newPoke.className = "pokecard";
+                    const link = document.createElement("a");
+                    link.setAttribute('href', "https://www.google.com.br/");
+                    const newPokediv = document.createElement("div");
+                    newPokediv.className = "pokeinfo";
+                    const idPoke = document.createElement("p");
+                    idPoke.innerText = data.id;
+                    const namePoke = document.createElement("h2");
+                    namePoke.innerText = data.name;
+
+
+                    newPokediv.appendChild(idPoke);
+                    newPokediv.appendChild(namePoke);
+
+                    var i = 0;
+                    data.types.map((currentType)=> {
+                    const typePoke = document.createElement("span");
+                    typePoke.innerText = currentType.type.name;
+                    newPokediv.appendChild(typePoke);
+                    if(i == 0 ){
+                        changeColor(currentType.type.name,typePoke,newPoke);
+                        i = i+ 1;
+                    }
+                    else {
+                        changeColorSpan(currentType.type.name,typePoke);
+                    }
+                    
+                    return currentType.type.name
+                });
+                const imagePoke = document.createElement("img");
+                
+                
+                imagePoke.setAttribute('src', `../assets/pokemon/${data.id}.svg`);
+
+                newPoke.appendChild(newPokediv);
+
+            if(data.id < 650){
+
+                newPoke.appendChild(imagePoke);
+            }
+          
+            link.appendChild(newPoke);
+
+            ul.appendChild(link);
+                
+                return data;
+            });
+
+        };
+       
+            
+    });
+         global +=20;   
+
+}
+
+
+
+
 
 
 
